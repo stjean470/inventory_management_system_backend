5258168bda04d967579133a308dbb77a6c7721dd
Steevenson Jean <steevej470@gmail.com>
2025-11-19 23:51:13 -0500

    Added a Item constructor not container List of Warehouses

--- src/main/java/com/skillstorm/inventory_management_system/controllers/ItemController.java
+++ src/main/java/com/skillstorm/inventory_management_system/controllers/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/items")
 public class ItemController {
 
